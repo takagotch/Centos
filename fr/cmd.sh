ede0f7dabca26b97a09e9af08eee7d1b63d053b7
takagotch <dyaccb@gmail.com>
2019-11-03 14:07:57 +0900

    nmcli device show

--- fr/cmd.sh
+++ fr/cmd.sh
@@ -1,5 +1,112 @@
+## syslog   journald rsyslog
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+cd 
+ls -F mv sosreport-centos70n02-20141022103807 /var/www/html
+systemctl start httpd
+ifrewall-cmd --list-all
+firewall-cmd --zone=public -add-service-http --permanent
+firewall-cmd --reload
+
+cd /var/www/html/
+chomd 755 ./sosreport-centos70n02-20141022103807
+cd ./sosreport-centos70n02-20141022103807
+chomd -R 655 ./sos_commands
+chomd -R 655 ./sos_logs
+chomd -R 655 ./sos_reports
+
+
+
+## NetworkManager
+
+ls -l /sys/class/net/
+ls -l /sys/class/net/
+
+nmcli connection
+
+nmcli connection down eno1
+nmcli connection show
+nmcli connection up eno1
+nmcli device
+nmcli device show
+nmcli device show eno1
+
+nmcli connection modify eno1 ipv4.addresses "10.0.0.82/24 10.0.0.1"
+nmcli connection down eno1 && nmcli connection up eno1
+
+nmcli device show eno1
+
+nmcli connection modify eno1 ipv4.dns "10.0.0.254 10.0.0.253"
+nmcli connection modify eno1 ipv4.routes "10.0.0.0/24 10.0.0.1"
+nmcli connection down eno1 && nmcli connection up eno1
+nmcli device show eno1
+
+nmcli device show eno1 | grep DNS
+cat /etc/resolv.conf
+vi /etc/NetworkManager/NetworkManager.conf
+nmcli connection modify eno1 ipv4.dns "10.0.0.253"
+
+nmcli connection down eno1 && nmcli connection up eno1
+nmcli device show eno1 | grep DNS
+systemctl restart network
+
+cat /etc/resolv.conf
+nmcli device
+nmcli connection delete eno1
+nmcli device
+
+nmcli connection add type ehternet ifname eno1 con-name eno1
+nmcli device
+
+nmcli connection modify eno1 ipv4.method manual ipv4.addresses "192.168.0.82"
+nmcli connection down eno1 && nmcli connection up eno1
+nmcli device show eno1
+cat /etc/sysconfig/network-scripts/ifcfg-eno1
+
+nmcli general hostname
+nmcli general hostname centos70n254.jpn.linux.hp.com
+hostname
+cat /etc/hostname
+
+
+nmcli networking
+nmcli networking on
+ip link
+nmcli networking off
+nmcli networking
+ip link
+
+nmtui
+
+
+ip a
+
+ip addr add 192.168.0.82/255.255.0 dev eno3
+ip add show eno3
+
+ip link
+
+ip route add default via 192.168.0.254
+ip route del default via 192.168.0.254
+
+ip neigh
+ip neigh flush 172.16.1.115
+
+ip -a l
+ss -ant # tcp socket ss
+ss -anu
 
 
 yum install -y teamd
